083542d645cf67e938ce1bcffe4efc44c5a78416
Yoon Koh <ykoh2@chicagobooth.edu>
2023-04-25 07:43:14 +0000

    done with directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -20,6 +20,7 @@ class DirectorsController < ApplicationController
     the_id = params.fetch("an_id")
 
     @the_director = Director.where({ :id => the_id }).at(0)
+    @filmography = Movie.where({ :director_id => the_id })
 
     render({ :template => "director_templates/show.html.erb"})
   end
